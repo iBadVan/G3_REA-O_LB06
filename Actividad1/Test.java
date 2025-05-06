9fe1fc294b1940cd4985f91a5c45433012f141f2
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-06 16:50:13 -0500

    creacion de la pila

--- Actividad1/Test.java
+++ Actividad1/Test.java
@@ -2,6 +2,21 @@ package Actividad1;
 
 public class Test {
     public static void main(String[] args) {
+        Stack<Integer> pilaEnteros = new StackArray<>(5);
+        System.out.println("PILA DE ENTEROS: ");
+
+        try {
+            pilaEnteros.push(1);
+            pilaEnteros.push(2);
+            pilaEnteros.push(3);
+            System.out.println("Contenido: " + pilaEnteros);
+            System.out.println("Top: " + pilaEnteros.top());
+            System.out.println("Pop: " + pilaEnteros.pop());
+            System.out.println("Contenido después del pop: " + pilaEnteros);
+        } catch (ExceptionIsEmpty e) {
+            System.err.println("Error: " + e.getMessage());
+        }
         
+
     }
 }
