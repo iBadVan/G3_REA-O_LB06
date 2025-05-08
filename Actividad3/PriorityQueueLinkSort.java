a4b6bf2b413b221682f61901f4300440627c62f1
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 23:06:45 -0500

    Update PriorityQueueLinkSort.java

--- Actividad3/PriorityQueueLinkSort.java
+++ Actividad3/PriorityQueueLinkSort.java
@@ -1,5 +1,7 @@
 package Actividad3;
 
+import Actividad1.ExceptionIsEmpty;
+
 public class PriorityQueueLinkSort <E, N extends Comparable<N>> implements PriorityQueue<E, N> {
     class EntryNode {
         E data;
@@ -23,5 +25,40 @@ public class PriorityQueueLinkSort <E, N extends Comparable<N>> implements Prior
         this.last = null;
     }
 
+    @Override
+    public void enqueue(E x, N pr) {
+        EntryNode entry = new EntryNode(x, pr);
+        Node<EntryNode> newNode = new Node<>(entry);
+
+        if (isEmpty()) {
+            first = last = newNode;
+        } else if (pr.compareTo(first.getData().priority) > 0) {
+            newNode.setNext(first);
+            first = newNode;
+        } else {
+            Node<EntryNode> current = first;
+            while (current.getNext() != null && pr.compareTo(current.getNext().getData().priority) <= 0) {
+                current = current.getNext();
+            }
+            newNode.setNext(current.getNext());
+            current.setNext(newNode);
+            if (newNode.getNext() == null) {
+                last = newNode;
+            }
+        }
+    }
+
+    @Override
+    public E dequeue() throws ExceptionIsEmpty {
+        if (isEmpty()) {
+            throw new ExceptionIsEmpty("Cannot remove from an empty queue");
+        }
+        E aux = first.getData().data;
+        first = first.getNext();
+        if (first == null) last = null;
+        return aux;
+    }
+
     
+
 }
