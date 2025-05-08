b4a8a52fcfa0a3054dd43b4c0b2795a360e0ab45
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 23:40:10 -0500

    Update Test.java

--- Ejercicio4/Test.java
+++ Ejercicio4/Test.java
@@ -2,6 +2,17 @@ package Ejercicio4;
 
 public class Test {
     public static void main(String[] args) {
-        
+        String[] entradas = {
+            "(()()())[()]", 
+            "(((())))[]", 
+            "([)][](", 
+            "([{])}", 
+            "[", 
+            "[[][]]{{{}}}"
+        };
+
+        for (String entrada : entradas) {
+            System.out.printf("Entrada: %-20s Resultado: %s%n", entrada, Application.symbolBalancing(entrada));
+        }
     }
 }
