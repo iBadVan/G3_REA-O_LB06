b4a476b91261990fdf13a386827a13acf02560ef
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 23:09:13 -0500

    Update Test.java

--- Actividad3/Test.java
+++ Actividad3/Test.java
@@ -1,7 +1,30 @@
 package Actividad3;
 
+import Actividad1.ExceptionIsEmpty;
+
 public class Test {
     public static void main(String[] args) {
+        PriorityQueue<String, Integer> colaMensajes = new PriorityQueueLinkSort<>();
+
+        System.out.println("== COLA DE PRIORIDAD: MENSAJES ==");
+
+        try {
+            colaMensajes.enqueue("Mensaje A", 2);
+            colaMensajes.enqueue("Mensaje B", 5);
+            colaMensajes.enqueue("Mensaje C", 3);
+            colaMensajes.enqueue("Mensaje D", 5); // misma prioridad que B
+            colaMensajes.enqueue("Mensaje E", 1);
+
+            System.out.println("Contenido: " + colaMensajes);
+            System.out.println("Front (mayor prioridad): " + colaMensajes.front());
+            System.out.println("Back (menor prioridad): " + colaMensajes.back());
+
+            System.out.println("Dequeue: " + colaMensajes.dequeue());
+            System.out.println("Después del dequeue: " + colaMensajes);
+
+        } catch (ExceptionIsEmpty e) {
+            System.err.println("Error: " + e.getMessage());
+        }
         
     }
 
