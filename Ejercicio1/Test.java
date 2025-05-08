61acf57fac0f5868ef407b49ca19f2d11379b038
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 23:17:42 -0500

    Update Test.java

--- Ejercicio1/Test.java
+++ Ejercicio1/Test.java
@@ -1,7 +1,25 @@
 package Ejercicio1;
 
+import Actividad1.ExceptionIsEmpty;
+
 public class Test {
     public static void main(String[] args) {
+        Stack<Integer> pilaEnteros = new StackLink<>();
+        System.out.println("PILA DE ENTEROS (LISTA ENLAZADA): ");
+        try {
+            pilaEnteros.push(100);
+            pilaEnteros.push(200);
+            pilaEnteros.push(300);
+            System.out.println("Contenido: " + pilaEnteros);
+            System.out.println("Top: " + pilaEnteros.top());
+            System.out.println("Pop: " + pilaEnteros.pop());
+            System.out.println("Contenido luego del pop: " + pilaEnteros);
+        } catch (ExceptionIsEmpty e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        System.out.println(" ");
         
+
     }
 }
