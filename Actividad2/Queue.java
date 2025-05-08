c77d2dc88131d69a5dffd9fc40bc6092dac71843
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 22:43:23 -0500

    implementacion node

--- Actividad2/Queue.java
+++ Actividad2/Queue.java
@@ -1,5 +1,11 @@
 package Actividad2;
 
-public class Queue {
-    
+import Actividad1.ExceptionIsEmpty;
+
+public interface Queue<E> {
+    void enqueue(E x);
+    E dequeue() throws ExceptionIsEmpty;
+    E front() throws ExceptionIsEmpty;
+    E back() throws ExceptionIsEmpty;
+    boolean isEmpty();
 }
