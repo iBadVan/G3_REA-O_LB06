923711adbe8909b1680cf71e141e5da5cd2e6de6
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 22:46:11 -0500

    Update QueueLink.java

--- Actividad2/QueueLink.java
+++ Actividad2/QueueLink.java
@@ -4,5 +4,11 @@ public class QueueLink<E> implements Queue<E> {
     private Node<E> first;
     private Node<E> last;
 
+    public QueueLink() {
+        this.first = null;
+        this.last = null;
+    }
+
+    
         
 }
