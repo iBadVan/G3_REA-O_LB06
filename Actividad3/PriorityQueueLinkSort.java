d4e011558bd2076b2d8b64d98aa2128eb18dca41
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 23:04:38 -0500

    Create PriorityQueueLinkSort.java

---
+++ Actividad3/PriorityQueueLinkSort.java
@@ -0,0 +1,27 @@
+package Actividad3;
+
+public class PriorityQueueLinkSort <E, N extends Comparable<N>> implements PriorityQueue<E, N> {
+    class EntryNode {
+        E data;
+        N priority;
+
+        EntryNode(E data, N priority) {
+            this.data = data;
+            this.priority = priority;
+        }
+
+        public String toString() {
+            return data + "(" + priority + ")";
+        }
+    }
+
+    private Node<EntryNode> first;
+    private Node<EntryNode> last;
+
+    public PriorityQueueLinkSort() {
+        this.first = null;
+        this.last = null;
+    }
+
+    
+}
