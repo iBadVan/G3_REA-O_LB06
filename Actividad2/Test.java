928cd69415d9cc22fce692b060cbc6b32a0635be
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 22:49:48 -0500

    Update Test.java

--- Actividad2/Test.java
+++ Actividad2/Test.java
@@ -1,7 +1,22 @@
 package Actividad2;
 
+import Actividad1.ExceptionIsEmpty;
+
 public class Test {
     public static void main(String[] args) {
-        
+         Queue<Integer> colaEnteros = new QueueLink<>();
+        System.out.println("== COLA DE ENTEROS ==");
+        try {
+            colaEnteros.enqueue(10);
+            colaEnteros.enqueue(20);
+            colaEnteros.enqueue(30);
+            System.out.println("Contenido: " + colaEnteros);
+            System.out.println("Front: " + colaEnteros.front());
+            System.out.println("Back: " + colaEnteros.back());
+            System.out.println("Dequeue: " + colaEnteros.dequeue());
+            System.out.println("Contenido tras dequeue: " + colaEnteros);
+        } catch (ExceptionIsEmpty e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
