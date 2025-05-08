b3fd00aa3d874560508f8977641bbadc6f64124c
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 23:25:43 -0500

    Update Test.java

--- Ejercicio2/Test.java
+++ Ejercicio2/Test.java
@@ -1,7 +1,24 @@
 package Ejercicio2;
 
+import Actividad1.ExceptionIsEmpty;
+
 public class Test {
     public static void main(String[] args) {
+        Queue<Integer> colaEnteros = new QueueArray<>(5);
+        System.out.println("== COLA DE ENTEROS (ARREGLO) ==");
+        try {
+            colaEnteros.enqueue(10);
+            colaEnteros.enqueue(20);
+            colaEnteros.enqueue(30);
+            System.out.println("Contenido: " + colaEnteros);
+            System.out.println("Front: " + colaEnteros.front());
+            System.out.println("Back: " + colaEnteros.back());
+
+            System.out.println("Dequeue: " + colaEnteros.dequeue());
+            System.out.println("Contenido después del dequeue: " + colaEnteros);
+        } catch (ExceptionIsEmpty e) {
+            System.err.println("Error: " + e.getMessage());
+        }
         
     }
 }
