b54949d10899d07300ebb4be309ef15e8018be6c
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 23:30:27 -0500

    Update PriorityQueueLinked.java

--- Ejercicio3/PriorityQueueLinked.java
+++ Ejercicio3/PriorityQueueLinked.java
@@ -1,8 +1,19 @@
 package Ejercicio3;
 
+import Actividad2.QueueLink;
 import Actividad3.PriorityQueue;
 
 public class PriorityQueueLinked<E> implements PriorityQueue<E, Integer> {
+    private QueueLink<E>[] queues;
+    private int levels;
+
+    public PriorityQueueLinked(int levels) {
+        this.levels = levels;
+        queues = (QueueLink<E>[]) new QueueLink[levels];
+        for (int i = 0; i < levels; i++) {
+            queues[i] = new QueueLink<>();
+        }
+    }
     
     
 }
