2e5691008bbc023ab14349da541d80371b0e24e3
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 23:21:50 -0500

    Update QueueArray.java

--- Ejercicio2/QueueArray.java
+++ Ejercicio2/QueueArray.java
@@ -1,5 +1,17 @@
 package Ejercicio2;
 
 public class QueueArray<E> implements Queue<E> {
-    
+    private E[] array;
+    private int first;
+    private int last;
+    private int size;
+    private int capacity;
+
+    public QueueArray(int capacity) {
+        this.capacity = capacity;
+        this.array = (E[]) new Object[capacity];
+        this.first = 0;
+        this.last = -1;
+        this.size = 0;
+    }
 }
