486444cc963ba7dc69fc03630c669ea97dc8edec
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-07 23:00:27 -0500

    Update Node.java

--- Actividad3/Node.java
+++ Actividad3/Node.java
@@ -3,4 +3,21 @@ package Actividad3;
 public class Node<T> {
     private T data;
     private Node<T> next;
+    
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public Node<T> getNext() {
+        return this.next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
 }
